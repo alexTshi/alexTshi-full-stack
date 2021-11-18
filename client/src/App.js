26c074f30e12e5aa9bdd1ea235e940a6a6389685
Alex <alextshiswaka@gmail.com>
2021-11-17 19:36:04 -0500

    Added setAuthToken to for token

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,16 @@ import Navbar from "./components/Layout/Navbar";
 import Login from "./components/auth/Login";
 import Alert from "./components/Layout/Alert";
 import Register from "./components/auth/Register";
+import setAuthToken from "./utils/setAuthToken";
 //Redux 
 import {Provider} from 'react-redux';
 import store from './store'
 import "./App.css";
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
 function App() {
   return(
     <div>
