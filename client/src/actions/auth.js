a60f6470de938f2a0d5f369e13be898f8cb286a4
Alex <alextshiswaka@gmail.com>
2021-11-18 14:58:48 -0500

    Added more components

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,46 +1,79 @@
-import axios from 'axios';
+import api from '../utils/api';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
-import setAuthToken from '../utils/setAuthToken'
-//
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
+} from './types';
+
+// Load User
 export const loadUser = () => async dispatch => {
-    if(localStorage.token){
-        setAuthToken(localStorage.token);
-    }
-    try {
-        const res = await axios.get('/api/auth');
+  try {
+    const res = await api.get('/auth');
 
-        dispatch({ 
-            type: USER_LOADED,
-            payload: res.data
-        });
-    } catch (error) {
-        dispatch({
-            type: AUTH_ERROR
-        });
-    }
-}
-//Register User
-export const register = ({ name, email, password }) => async dispatch => {
-    const config = {
-        headers: { 'Content-Type': 'application/json' }
-    }
-    const body = JSON.stringify({name, email, password});
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
-    try {
-        const res = await axios.post('api/users', body, config);
-        dispatch({ 
-            type: REGISTER_SUCCESS,
-            payload: res.data
-        });
-    } catch (err) {
-        const errors = err.message.data.errors;
+// Register User
+export const register = formData => async dispatch => {
+  try {
+    const res = await api.post('/users', formData);
 
-        if(errors){
-            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
-        }
-        dispatch({ 
-            type: REGISTER_FAIL
-        })
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }
-}
+
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+  const body = { email, password };
+
+  try {
+    const res = await api.post('/auth', body);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
+
+// Logout
+export const logout = () => ({ type: LOGOUT });
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,69 +1,86 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
-      name: '',
-      email: '',
-      password: '',
-      password2: ''
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
   });
-  const {name, email, password, password2} = formData;
-  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
-  const onSubmit = async e => {
-      e.preventDefault();
-      if(password !== password2) {
-        setAlert('Passwords do not match!', 'danger');
-      }
-      else{
-        register({ name, email, password });
-      }
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      register({ name, email, password });
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
   }
+
   return (
     <Fragment>
-      <h1 class="large text-primary">Sign Up</h1>
-      <p class="lead">
-        <i class="fas fa-user"></i> Create Your Account
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Create Your Account
       </p>
-      <form class="form" onSubmit={e => onSubmit(e)}>
-        <div class="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
         </div>
-        <div class="form-group">
-          <input type="email" placeholder="Email Address" name="email" 
-            value={email} onChange={e => onChange(e)} required />
-          <small class="form-text">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Password"
             name="password"
-            minLength="6"
             value={password}
-            onChange={e => onChange(e)}
+            onChange={onChange}
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input
             type="password"
             placeholder="Confirm Password"
             name="password2"
             value={password2}
-            minLength="6"
-            onChange={e => onChange(e)}
+            onChange={onChange}
           />
         </div>
-        <input type="submit" class="btn btn-primary" value="Register" />
+        <input type="submit" className="btn btn-primary" value="Register" />
       </form>
-      <p class="my-1">
+      <p className="my-1">
         Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
@@ -72,7 +89,12 @@ const Register = ({ setAlert, register }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
-}
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
-export default connect(null, {setAlert, register})(Register);
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
